4f8ca20e30168005a36b1c51c4e86e0e9df4fc9a
Ramita20 <ramiro_larrea18@hotmail.com>
2024-11-10 19:31:45 -0300

    Comenzando con el proyecto.

---
+++ src/main/java/com/literalura/catalogo_libros/CatalogoLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.literalura.catalogo_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoLibrosApplication.class, args);
+	}
+
+}
